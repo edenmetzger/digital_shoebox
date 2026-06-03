ce824839fcdfa093ec9f1f04f3b729e1509ae1bf
Eden Metzger <56650649+edenmetzger@users.noreply.github.com>
2026-06-03 17:47:19 -0500

    Add files via upload

--- blurbs.js
+++ blurbs.js
@@ -1,11 +1,61 @@
 const blurbs = {
   "scan1.jpg": {
-    title: "Grandfather's Workshop",
-    description: "Photograph of my grandfather's workshop in Detroit."
+    title: "le nain rouge patch",
+    description: "fabled detroit creature said to bring misfortune to white settlers in the area"
   },
 
   "scan2.jpg": {
-    title: "Pokémon Tournament Card",
-    description: "Trading card acquired during my first tournament."
+    title: "MTA MARC ticket",
+    description: "baltimore to washington, d.c. train ticket from 2021"
+  },
+
+  "scan3.jpg": {
+    title: "open gesture of an i by d.a. powell",
+    description: "poem from a 2019 issue of the new yorker magazine"
+  },
+
+  "scan4.jpg": {
+    title: "GQ style profile",
+    description: "kurt cobain's 2021 GQ style profile"
+  },
+
+  "scan5.jpg": {
+    title: "learn spanish with bazooka joe",
+    description: "cómic educativo de Bazooka Joe"
+  },
+
+  "scan6.jpg": {
+    title: "collage materials",
+    description: "various body parts"
+  },
+
+  "scan7.jpg": {
+    title: "schlagenheim sticker",
+    description: "from the plastic sleeve of black midi's 2019 album schlagenheim"
+  },
+
+  "scan8.jpg": {
+    title: "3d glasses",
+    description: "from the belcourt's summer 2023 showing of 'Creature from the Black Lagoon' in 3D"
+  },
+
+  "scan9.jpg": {
+    title: "darkrai pokémon card",
+    description: "found at a halloween party"
+  },
+
+  "scan10.jpg": {
+    title: "vigilante coffee bag",
+    description: "the front panel of a paper bag from a hyattsville, md coffee shop"
+  },
+
+  "scan11.jpg": {
+    title: "hand wipe",
+    description: "from rendezvous bbq in memphis, tn"
+  },
+
+  "scan12.jpg": {
+    title: "'some butterflies' by jack goldstein",
+    description: "postcard from c/o berlin"
   }
 };
